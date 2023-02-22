d81efeaa4f00c3ab894f30662eb7b1f378e38b89
Jessica Ríos Maneiro <117263066+JessRm04@users.noreply.github.com>
2023-02-23 00:13:56 +0100

    add generic button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import { Route, Routes } from "react-router";
 import "./App.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import GenericDropdown from "./assets/atom-dropdown/atom-dropdown";
+import GenericButton from "./assets/atom-button/atom-button";
 
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     // <Routes>
     //   <Route path="/" element={''}/>
     // </Routes>
-    <GenericDropdown title="Tu experiencia" option1="Junior" option2="Mid" option3="Senior"/>
+    <GenericButton/>
   );
 }
 
