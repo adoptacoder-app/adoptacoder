38e2646fea9c4952f7b61df4b5574d9dcbc4c867
Jessica Ríos Maneiro <117263066+JessRm04@users.noreply.github.com>
2023-02-23 00:58:28 +0100

    add name, last name, mail, and avatar fields

--- src/assets/mol-form/form.jsx
+++ src/assets/mol-form/form.jsx
@@ -7,24 +7,34 @@ function AdoptMeForm() {
     <div className="form">
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Elige un avatar</Form.Label>
+        <Form.Label>Selecciona tu foto de perfil</Form.Label>
         <Form.Control type="file" />
+        <Form.Label>First name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="¿Lucía? ¿Marcos?"
+          />
+        <Form.Label>Last name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="¿Pérez?"
+          />
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
         <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
+          Tranquil@, no compartiremos tu mail con nadie más.<br></br>
         </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
+
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
       </Form.Group>
       <Button variant="primary" type="submit">
-        Submit
+        ¡Adóptame!
       </Button>
     </Form>
     </div>
