794cca017cdfedd76358cef679b299a38927d76b
mauisiri <irisleo.com@gmail.com>
2023-02-23 15:00:16 +0100

    folder changed and link updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import { Route, Routes } from "react-router";
 import "./App.css"
-import AtomNavbar from "./assets/navbar/AtomNavbar";
+import AtomNavbar from "./Components/atom-navbar/AtomNavbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import GenericCard from "./assets/atom-card/atom-card";
 import { Route, Routes } from "react-router";
@@ -9,9 +9,12 @@ import Home from "./Pages/Home/Home";
 
 function App() {
   return (
-    <Routes>
+    <>
+    <AtomNavbar/>
+    </>
+    /*<Routes>
       <Route path="/" element={<Home/>}/>
-    </Routes>
+    </Routes>*/
     // <GenericCard name="Carlos" bio="Some quick example text to build on the card title and make up the
     // bulk of the card's content."/>
   );
