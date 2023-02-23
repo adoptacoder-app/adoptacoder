892d02458899387e313dd0a2e262c92df120af35
TaniaPandal <psiqonautas@gmail.com>
2023-02-23 19:19:07 +0100

    AtomBio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-// import { Route, Routes } from "react-router";
-import "./App.css"
-import AtomNavbar from "./Components/atom-navbar/AtomNavbar";
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import GenericCard from "./assets/atom-card/atom-card";
 import { Route, Routes } from "react-router";
-import Home from "./Pages/Home/Home";
-
+import "./App.css"
 
 function App() {
   return (
-    <>
-    <AtomNavbar/>
-    </>
-    /*<Routes>
-      <Route path="/" element={<Home/>}/>
-    </Routes>*/
-    // <GenericCard name="Carlos" bio="Some quick example text to build on the card title and make up the
-    // bulk of the card's content."/>
+    <Routes>
+      <Route path="/" element={''}/>
+    </Routes>
   );
 }
-
-
 
 export default App;
