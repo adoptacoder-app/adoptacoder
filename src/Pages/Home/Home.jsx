ea0404b9df60b4f6b3c17f0f3cf1c6559b22fa6e
SaidGM <said.mg.1517@gmail.com>
2023-02-23 13:01:17 +0100

    home

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './HomeStyle.css'
+
+function Home() {
+  return (
+    <>
+    <nav className='nav'>
+      
+    </nav>
+    <main>
+        <section className='hero'>
+            <div className='title'>
+             <h1>Encuentra <br/> <span>coders</span> <br/>para tu proyecto</h1>
+            </div>
+            <div className='caption'>
+              <button className='button'> ¡Adopta! {'>>'} </button>
+              <h4>¡Eres coder?</h4>
+              <h4>¡Hazte adoptar! {'>>'} </h4>
+            </div>
+        </section>
+    </main>
+    </>
+    
+  )
+}
+
+export default Home
