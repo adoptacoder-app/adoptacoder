d8ac53ea580cdefdfd4296e386953847485c6ddd
SaidGM <said.mg.1517@gmail.com>
2023-02-23 15:02:28 +0100

    update

---
+++ src/Pages/Home/Components/Cards.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Cards() {
+  return (
+      <section className='cards'>
+          <div>
+              <h3>Coder Destacados</h3>
+          </div>
+          <article>
+
+          </article>
+      </section>
+  )
+}
+
+export default Cards
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,28 +1,17 @@
 import React from 'react'
+import Cards from './Components/Cards'
+import Hero from './Components/Hero'
+import SelecCoder from './Components/SelecCoder'
 import './HomeStyle.css'
 
 function Home() {
   return (
     <>
-    <nav className='nav'>
-      
-    </nav>
+    <nav></nav>
     <main>
-        <section className='hero'>
-            <div className='title'>
-             <h1>Encuentra <br/> <span>coders</span> <br/>para tu proyecto</h1>
-            </div>
-            <div className='caption'>
-              <button className='buttonAdopta'> ¡Adopta! {'>>'} </button>
-              <div>
-                <h6>¡Eres coder?</h6>
-                <h6>¡Hazte adoptar! {'>>'} </h6>
-              </div> 
-            </div>
-        </section>
-        <section>
-          
-        </section>
+        <Hero/>
+        <SelecCoder/>
+        <Cards/>
     </main>
     </>
     
