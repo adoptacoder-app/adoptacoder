2c4eae65678f009cae8973c738087df7826e2846
Gabriela M. Fernández <galalou2010@gmail.com>
2023-02-23 18:26:59 +0100

    setting up files techIcons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import { Route, Routes } from "react-router";
-import "./App.css"
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detailsProfile/:id" element={<CoderProfile />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/detailsProfile/:id" element={<CoderProfile />} />
+    </Routes>
   );
 }
 
