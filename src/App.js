c854bd6e737bdf83c88e926c9cc2b1c2fa108539
Gabriela M. Fernández <galalou2010@gmail.com>
2023-02-23 15:52:32 +0100

    photo changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 //import CoderSnippet from "./assets/atom-coderSnippet/atom-coderSnippet";
 //import CodersList from "./assets/mol-codersList/mol-codersList";
-import CoderProfile from "./assets/mol-coderProfile/mol-coderProfile";
+import CoderProfile from "./Components/mol-coderProfile/mol-coderProfile";
 
 function App() {
   return (
