e1ab3f006c2e4e65cebe955286add3bafb732103
SaidGM <said.mg.1517@gmail.com>
2023-02-23 21:58:29 +0100

    Update

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import AtomNavbar from '../../Components/atom-navbar/AtomNavbar'
-import Cards from '../../Components/components-home/Cards'
-import Hero from '../../Components/components-home/Hero'
+import Hero from './Components-home/Hero'
+import Cards from './Components-home/Cards'
 import './HomeStyle.css'
 
 function Home() {
