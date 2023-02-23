6ca0ad6d68959e18503a9de83a02801dab6d2cc9
Jessica Ríos Maneiro <117263066+JessRm04@users.noreply.github.com>
2023-02-23 18:20:30 +0100

    Fix App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { Route, Routes } from "react-router";
 import "./App.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "./Pages/Home/Home.jsx"
+import CoderProfile from "./Components/mol-coderProfile/mol-coderProfile.jsx"
 
 
 function App() {
   return (
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
         <Route path="/detailsProfile/:id" element={<CoderProfile />} />
       </Routes>
-    </>
   );
 }
 
