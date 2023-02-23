9202b537fe6ece959abf4c98a0379096c81ee6cc
mauisiri <irisleo.com@gmail.com>
2023-02-23 12:20:23 +0100

    img of languajes

--- src/assets/atom-back/atom-back.jsx
+++ src/assets/atom-back/atom-back.jsx
@@ -1,12 +1,16 @@
-import Button from 'react-bootstrap/Button';
+/*import Button from 'react-bootstrap/Button';
 import "./atom-button.css"
+import { useParams, useNavigate } from 'react-router-dom'
+
+const history = useNavigate();
+
 
 function BackButton() {
   return (
     <>
-      <Button>Back</Button>{' '}
+            <Button onClick={()=> history(-1)}>Back</Button>
     </>
   );
 }
 
-export default GenericButton;
+export default GenericButton;*/
