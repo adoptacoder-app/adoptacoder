0ddd83ee27b8796ba0af269c247677e04deb1f12
SaidGM <said.mg.1517@gmail.com>
2023-02-23 13:30:43 +0100

    update

--- src/assets/mol-codersList/mol-codersList.jsx
+++ src/assets/mol-codersList/mol-codersList.jsx
@@ -16,7 +16,7 @@ function CodersList() {
   return (
     <div className="coderList-container">
         {data.slice(0, 10).map(item => (
-        <GenericCard key={item} img={item.avatar} name={item.firstName} bio={item.bio}/>
+        <GenericCard key={item.id} img={item.avatar} name={item.firstName} bio={item.bio}/>
       ))}
     </div>
   )
