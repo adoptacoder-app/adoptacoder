fc7632bf71e7e8b7839033cc149ee10dfdf72d4e
SaidGM <said.mg.1517@gmail.com>
2023-02-23 12:33:37 +0100

    carpetas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 // import { Route, Routes } from "react-router";
 import "./App.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import GenericCard from "./assets/atom-card/atom-card";
+// import GenericCard from "./assets/atom-card/atom-card";
+import { Route, Routes } from "react-router";
+import Home from "./Pages/Home/Home";
 
 
 function App() {
   return (
-    // <Routes>
-    //   <Route path="/" element={''}/>
-    // </Routes>
-    <GenericCard name="Carlos" bio="Some quick example text to build on the card title and make up the
-    bulk of the card's content."/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+    // <GenericCard name="Carlos" bio="Some quick example text to build on the card title and make up the
+    // bulk of the card's content."/>
   );
 }
 
