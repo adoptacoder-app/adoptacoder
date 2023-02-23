397505473f64718600ba00e86766847cbb6ba711
Jessica Ríos Maneiro <117263066+JessRm04@users.noreply.github.com>
2023-02-23 12:29:28 +0100

    add checklist

--- src/assets/mol-form/form.jsx
+++ src/assets/mol-form/form.jsx
@@ -1,6 +1,7 @@
 import './form.css'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import GenericDropdown from '../atom-dropdown/atom-dropdown';
 
 function AdoptMeForm() {
   return (
@@ -8,34 +9,55 @@ function AdoptMeForm() {
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Selecciona tu foto de perfil</Form.Label>
-        <Form.Control type="file" />
-        <Form.Label>First name</Form.Label>
+        <Form.Control type="file" className="placeholderStyle"/>
+        <Form.Label>Nombre</Form.Label>
           <Form.Control
             required
             type="text"
             placeholder="¿Lucía? ¿Marcos?"
+            className="placeholderStyle"
           />
-        <Form.Label>Last name</Form.Label>
+        <Form.Label>Apellidos</Form.Label>
           <Form.Control
             required
             type="text"
             placeholder="¿Pérez?"
+            className="placeholderStyle"
           />
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" className="placeholderStyle"/>
         <Form.Text className="text-muted">
-          Tranquil@, no compartiremos tu mail con nadie más.<br></br>
         </Form.Text>
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Label>Contraseña</Form.Label>
+        <Form.Control type="password" placeholder="Password" className="placeholderStyle"/>
+        <Form.Label>Github</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Si tienes perfil en Github, ¡compártelo!"
+            className="placeholderStyle"
+          />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
+      <GenericDropdown className="boton" title="¿Qué experiencia tienes?" option1="Junior" option2="Mid" option3="Senior"/><br></br>
+      <GenericDropdown title="¿Qué tipo de coder eres?" option1="Frontend" option2="Backend" option3="Fullstack"/><br></br>
+
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Háblanos de ti</Form.Label>
+        <Form.Control as="textarea" rows={3} />
       </Form.Group>
-      <Button variant="primary" type="submit">
+      
+
+
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Utilizo metodologías ágiles" />
+        <Form.Check type="checkbox" label="Controlo de testing (TDD...)" />
+      </Form.Group>
+      <div className="submitButtonContainer">
+      <Button variant="primary" type="submit" className="submitButton">
         ¡Adóptame!
       </Button>
+      </div>
     </Form>
     </div>
   );
